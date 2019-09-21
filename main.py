e74358314b528d2fb22dda558e73d4204c8d9d3f
Roy Assis <royassisg@gmail.com>
2019-09-21 20:40:04 +0300

    functions to file

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ from datetime import datetime as  dt
 
 def getKmers(sequence, size):
     return [sequence[x:x+size].lower() for x in range(len(sequence) - size + 1)]
+
 def make_sentence(mySeq,word_size):
     words = getKmers(mySeq, size=word_size)
     sentence = ' '.join(words)
     return sentence
+
 def records_to_list(records):
     arr = []
     for record in records:
@@ -27,54 +29,57 @@ def records_to_list(records):
         taxo = record.annotations["taxonomy"][-5]
         arr.append([sentence,taxo])
     return arr
+
 def genebank_to_numpyarr(path):
     file_type = path.split(".")[1]
     records = SeqIO.parse(path, file_type)
     l = records_to_list(records)
     np_arr = np.asarray(l,dtype='U')
     return np_arr
+
 def write_to_file(file_name, array):
     with open(file_name, 'w') as f:
         f.write("n_components, mean_score, time \n")
         for i in array:
             i =[str(j) for j in i]
             f.write(",".join(i) + "\n")
-def timer(func):
-   def func_wrapper():
-       t1 = dt.now()
-       scores =  func()
-       t2 = dt.now()
-       delta = (t2 - t1).seconds
-       return delta, scores
-   return func_wrapper
-
-@timer
-def main_func():
-    pca = PCA(n_components=i)
-    X_copy = pca.fit_transform(X)
-    #y
-    le = LabelEncoder()
-    y = le.fit_transform(arr[:,1])
-    clf = SVC(gamma='auto')
-    scores = cross_val_score(clf, X_copy, y, cv=5).mean()
-    return scores
 
 
 
 path = "seq/Asparagales.gb"
 arr = genebank_to_numpyarr(path)
 
+
 #X
 cv = CountVectorizer()
 X = cv.fit_transform(arr[:,0]).toarray()
+le = LabelEncoder()
+y = le.fit_transform(arr[:, 1])
+clf = SVC(gamma='auto')
 
+def timer(func):
+   def func_wrapper(i,X,y,clf):
+       t1 = dt.now()
+       scores =  func(i,X,y,clf)
+       t2 = dt.now()
+       delta = (t2 - t1).seconds
+       return delta, scores
+   return func_wrapper
+
+@timer
+
+def main_func(i,X,y,clf):
+    pca = PCA(n_components=i)
+    X_copy = pca.fit_transform(X)
+    scores = cross_val_score(clf, X_copy, y, cv=5).mean()
+    return scores
 
 n=1
 m=3
 arr2=[]
 for i in range (n,m):
-    x,y = main_func()
-    arr2.append([i,x,y])
+    delta, scores = main_func(i,X,y,clf)
+    arr2.append([i,delta, scores])
     print(i)
 
 write_to_file("svn.txt", arr2)
