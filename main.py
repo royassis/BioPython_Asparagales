0d02bc713f840d008293d9072513756084efc71f
Roy Assis <royassisg@gmail.com>
2019-09-22 20:28:32 +0300

    tidey up and comments

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from functions import *
 from sklearn.model_selection import GridSearchCV
 import pickle
 
-
 #input params
 infile = "seq/Asparagales.gb"
 outfile = "svn.txt"
@@ -11,6 +10,7 @@ log_folder ="logs"
 n=1
 m=3
 
+#Get data from file
 data_array = genebank_to_numpyarr(infile, process_function_return_string)
 
 #Xy
@@ -18,14 +18,17 @@ X = data_array[:, 0]
 le = LabelEncoder()
 y = le.fit_transform(data_array[:, 1])
 
-
 #Learning
 cv = CountVectorizer()
 pca = TruncatedSVD(n_components=2)
 clf = SVC(gamma='auto',probability=True)
 T = Transformer()
 
-model_transformation = Pipeline([("Transformer",T),('CountVectorizer', cv), ("pca", pca), ('svc', clf)])
+model_transformation = Pipeline([("Transformer",T),
+                                 ('CountVectorizer', cv),
+                                 ("pca", pca),
+                                 ('svc', clf)])
+
 parameters = {"pca__n_components": [i for i in range(1,100,10)]}
 
 model_transformation = GridSearchCV(model_transformation, parameters, cv=5, verbose=2)
